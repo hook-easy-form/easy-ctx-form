1bc4309b666602f5dbd5251e315c02e6bfa5e811
Artem <artem.s@twsgo.com>
2021-03-28 17:28:20 +0300

    master change declaration generation

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import sourceMap from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
 // import { nodeResolve } from '@rollup/plugin-node-resolve';
 // import commonjs from '@rollup/plugin-commonjs';
-// import { uglify } from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
 import pkg  from './package.json';
 
@@ -10,13 +11,24 @@ export default {
   output: [
     {
       file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
       format: 'es',
+      exports: 'named',
       sourcemap: true,
     },
   ],
   plugins: [
     sourceMap(),
-    // uglify(),
+    typescript({
+      rollupCommonJSResolveHack: false,
+      clean: true,
+    }),
+    uglify(),
     // nodeResolve(),
     // commonjs(),
   ],
